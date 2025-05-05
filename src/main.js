20de4a3a4c0277f3240d38b3d9e10678fcb88f20
Nicolas Bes <nickfbes@gmail.com>
2025-05-05 23:46:39 +0200

    change color

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/styles/globais.css'
-import './assets/styles/reset.css'
-import './assets/styles/variaveis.css'
+import './assets/styles/main.css'
+
 
 // FontAwesome
 import { library } from '@fortawesome/fontawesome-svg-core'
